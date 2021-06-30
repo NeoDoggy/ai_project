00b15cf5c5e5b33098b2cbd25c76bc71b90a9f57
NeoDoggy <neodog0616@gmail.com>
2021-06-30 14:24:41 +0800

    trd upd

--- main.py
+++ main.py
@@ -2,37 +2,33 @@ from numpy import genfromtxt
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense
+from tensorflow import keras
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import numpy as np
 import pydot
 
-Dx=np.load('./datasets/traindataX.npy')
-Dy=np.load('./datasets/traindataY.npy')
-Tx=np.load('./datasets/testdataX.npy')
-Ty=np.load('./datasets/testdataY.npy')
+model=keras.models.load_model('fruit')
 
-Dx=Dx/255
+#making test dataset
+path="./numpydatasets"
+apple=np.load(f'{path}/apple.npy')
+banana=np.load(f'{path}/banana.npy')
+blackberry=np.load(f'{path}/blackberry.npy')
+blueberry=np.load(f'{path}/blueberry.npy')
+grapes=np.load(f'{path}/grapes.npy')
+pear=np.load(f'{path}/pear.npy')
+strawberry=np.load(f'{path}/strawberry.npy')
+'''
+Tx=np.concatenate((apple[-10:],banana[-10:]))
+Tx=np.concatenate((Tx,blackberry[-10:]))
+Tx=np.concatenate((Tx,blueberry[-10:]))
+Tx=np.concatenate((Tx,grapes[-10:]))
+Tx=np.concatenate((Tx,pear[-10:]))
+Tx=np.concatenate((Tx,strawberry[-10:]))
+'''
+Tx=strawberry[-100:]
 Tx=Tx/255
-
-Dy=np_utils.to_categorical(Dy,8)
-Ty=np_utils.to_categorical(Ty,8)
-
-model=Sequential()
-model.add(Dense(input_dim=28*28,units=256,activation='relu'))
-model.add(Dense(units=128,activation='relu'))
-model.add(Dense(units=8,activation='softmax'))
-model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-train_history=model.fit(x=Dx,
-                        y=Dy,
-                        validation_split=0.2,
-                        epochs=50,
-                        batch_size=600,
-                        verbose=2)
-#plt.plot(train_history.history['loss'])
-plt.plot(train_history.history['accuracy'])
-plt.show()
-model.evaluate(Tx,Ty,batch_size=50)
 prediction=model.predict_classes(Tx)
-print(prediction[:10])
+print(prediction)
---
+++ makemodel.py
@@ -0,0 +1,39 @@
+from numpy import genfromtxt
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import pydot
+
+Dx=np.load('./datasets/traindataX.npy')
+Dy=np.load('./datasets/traindataY.npy')
+Tx=np.load('./datasets/testdataX.npy')
+Ty=np.load('./datasets/testdataY.npy')
+
+Dx=Dx/255
+Tx=Tx/255
+
+Dy=np_utils.to_categorical(Dy,8)
+Ty=np_utils.to_categorical(Ty,8)
+
+model=Sequential()
+model.add(Dense(input_dim=28*28,units=256,activation='relu'))
+model.add(Dense(units=128,activation='relu'))
+model.add(Dense(units=8,activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+train_history=model.fit(x=Dx,
+                        y=Dy,
+                        validation_split=0.2,
+                        epochs=50,
+                        batch_size=600,
+                        verbose=2)
+#plt.plot(train_history.history['loss'])
+plt.plot(train_history.history['accuracy'])
+plt.show()
+model.evaluate(Tx,Ty,batch_size=50)
+prediction=model.predict_classes(Tx)
+print(prediction[:10])
+model.save('fruit')
